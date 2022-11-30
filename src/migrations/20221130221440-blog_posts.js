01b1dbabda744c76b099b48942cd4299611782b8
Reinaldo <reinaldoper83@gmail.com>
2022-11-30 20:07:09 -0300

    requisito 2

--- src/migrations/20221130221440-blog_posts.js
+++ src/migrations/20221130221440-blog_posts.js
@@ -17,11 +17,12 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING,
       },
-      user_id: {
+      userId: {
         type: Sequelize.INTEGER,
         references: {
           model: 'users',
           key: 'id',
+          field: 'user_id',
         },
         onDelete: 'CASCADE',
       },
--- src/migrations/20221130221717-post_categories.js
+++ src/migrations/20221130221717-post_categories.js
@@ -3,20 +3,22 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('posts_categories', {
-      post_id: {
+      postId: {
         type: Sequelize.INTEGER,
         references: {
           model: 'blog_posts',
           key: 'id',
+          field: 'post_id',
         },
         onDelete: 'CASCADE',
         primaryKey: true,
       },
-      category_id: {
+      categoryId: {
         type: Sequelize.INTEGER,
         references: {
           model: 'categories',
           key: 'id',
+          field: 'category_id',
         },
         onDelete: 'CASCADE',
         primaryKey: true,
