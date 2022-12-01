480fec58a0765f77a61726b4868b2fcda0b95178
Reinaldo <reinaldoper83@gmail.com>
2022-12-01 19:27:18 -0300

    requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const post = sequelize.define(
+    'PostCategory',
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true
+      },
+    },
+    { timestamps: false, underscored: true }
+  );
+
+  post.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogs',
+      through: post,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: post,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return post;
+};
